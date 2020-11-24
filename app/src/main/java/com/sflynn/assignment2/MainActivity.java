cde0420b30ccf811d20f29dd62dc17ff5d9aff27
18214869 <sabrinaflynn6@gmail.com>
2020-11-24 17:05:45 +0000

    Open Camera Complete

--- app/src/main/java/com/sflynn/assignment2/MainActivity.java
+++ app/src/main/java/com/sflynn/assignment2/MainActivity.java
@@ -2,7 +2,10 @@ package com.sflynn.assignment2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public void onClick (View view){
+        Intent openIntent = new Intent();
+        openIntent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivity(openIntent);
+
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+    }
+
+
 }
